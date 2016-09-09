69e6dcaef85fb3d4288ba3e75b1948b69221df2b
Joaco <j@florius.com.ar>
2016-09-08 23:04:49 -0300

    usar una página

--- src/main/java/com/tenpines/starter/HelloWorldController.java
+++ src/main/java/com/tenpines/starter/HelloWorldController.java
@@ -1,14 +1,14 @@
 package com.tenpines.starter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Otra cosa";
+    String home(Model model) {
+        model.addAttribute("mensaje", "Holitas!");
+        return "hola";
     }
 }
---
+++ src/main/resources/META-INF/resources/jsp/hola.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head lang="en">
+    <meta charset="UTF-8" />
+    <title>HELLO</title>
+</head>
+<body>
+    <h1>${mensaje}</h1>
+</body>
+</html>
