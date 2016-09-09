9a1f07e7c171925eefd8971c983f648128839edf
Joaco <j@florius.com.ar>
2016-09-08 22:11:10 -0300

    innitial commit :camel:

---
+++ src/main/java/com/tenpines/StarterMainRunner.java
@@ -0,0 +1,12 @@
+package com.tenpines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterMainRunner {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(StarterMainRunner.class, args);
+    }
+}
